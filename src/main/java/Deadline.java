14dfdd8bac22536f27ca347f4fe3f288af157f47
Han Yang <leowhanyang0905@gmail.com>
2026-08-27 23:41:45 +0800

    Add dates and times parsing

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,19 +1,30 @@
+/**
+ * Represents a task that should be completed by a particular time.
+ */
 public class Deadline extends Task {
-    String deadline;
+    Object deadline;
 
+    /**
+     * Creates a deadline, parsing date-times written as {@code yyyy-MM-dd HHmm}.
+     *
+     * @param title task description
+     * @param deadline deadline date-time or free-form text
+     */
     public Deadline(String title, String deadline) {
         super(title);
-        this.deadline = deadline;
+        this.deadline = DateTimeParser.parse(deadline);
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + this.deadline + ")";
+        return "[D]" + super.toString()
+                + " (by: " + DateTimeParser.formatForDisplay(this.deadline) + ")";
     }
 
     @Override
     public String toStorageFormat() {
         String mark = this.isDone ? "1" : "0";
-        return "D | " + mark + " | " + this.description + " | " + this.deadline;
+        return "D | " + mark + " | " + this.description + " | "
+                + DateTimeParser.formatForStorage(this.deadline);
     }
 }
